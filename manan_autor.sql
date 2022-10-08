ee4c1842d1de092f9cdaeb55595411fb0961bef4
Manan1612 <mananpatel161200@gmail.com>
2022-10-08 17:36:03 -0400

    updated

--- manan_autor.sql
+++ manan_autor.sql
@@ -1,8 +1,6 @@
 REM   Script: manan_autor
 REM   queries
 
-Create table CUSTOMER(c_id int NOT NULL, c_status int, c_name varchar(20), PRIMARY KEY (c_id)) 
-Create table OWNS(c_id int NOT NULL, vin int NOT NULL, FOREIGN KEY (c_id) REFERENCES CUSTOMER(c_id), FOREIGN KEY (vin) REFERENCES VEHICLE(vin)) 
-Create table ASSOCIATED_SERVICE_CENTER(c_id int NOT NULL, sc_id int NOT NULL, FOREIGN KEY (c_id) REFERENCES CUSTOMER(c_id), FOREIGN KEY (sc_id) REFERENCES SERVICE_CENTER(sc_id)) 
+Create table OWNS(c_id int NOT NULL, vin int NOT NULL, FOREIGN KEY (vin) REFERENCES VEHICLE(vin)) 
+Create table CUSTOMER_ASSOCIATED_SC(c_id int NOT NULL, c_status int, c_fame varchar(20), c_lame varchar(20), PRIMARY KEY(c_id, sc_id), sc_id int NOT NULL, FOREIGN KEY (sc_id) REFERENCES SERVICE_CENTER(sc_id)) 
 Create table SERVICE_EVENT(c_id int NOT NULL, sevent_id varchar NOT NULL, total_amount_paid int, amount_charged int, s_no int NOT NULL, mechanic_id int NOT NULL, FOREIGN KEY(s_no) REFERENCES SERVICES(s_no), FOREIGN KEY(c_id) REFERENCES CUSTOMER(c_id), FOREIGN KEY (mechanic_id) REFERENCES EMPLOYEE(emp_id));
-
